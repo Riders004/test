84ec74780c250c208d6cbe047b8983565d0454ab
Riders004 <120787636+Riders004@users.noreply.github.com>
2023-07-12 14:35:08 +0000

    new2

--- Core.js
+++ Core.js
@@ -29,14 +29,14 @@ global.db = new Low(
   /https?:\/\//.test(opts["db"] || "")
     ? new cloudDBAdapter(opts["db"])
     : /mongodb/.test(opts["db"])
-    ? new mongoDB(opts["db"])
-    : new JSONFile(`src/database.json`)
+      ? new mongoDB(opts["db"])
+      : new JSONFile(`src/database.json`)
 );
 global.DATABASE = global.db; // Backwards Compatibility
 global.loadDatabase = async function loadDatabase() {
   if (global.db.READ)
     return new Promise((resolve) =>
-      setInterval(function () {
+      setInterval(function() {
         !global.db.READ
           ? (clearInterval(this),
             resolve(
@@ -76,9 +76,9 @@ if (global.db)
 let banUser = JSON.parse(fs.readFileSync('./database/banUser.json'));
 let banchat = JSON.parse(fs.readFileSync('./database/banChat.json'));
 
- let _limit = JSON.parse(fs.readFileSync('./storage/user/limit.json'));
- let _buruan = JSON.parse(fs.readFileSync('./storage/user/bounty.json'));
- let _darahOrg = JSON.parse(fs.readFileSync('./storage/user/blood.json'))
+let _limit = JSON.parse(fs.readFileSync('./storage/user/limit.json'));
+let _buruan = JSON.parse(fs.readFileSync('./storage/user/bounty.json'));
+let _darahOrg = JSON.parse(fs.readFileSync('./storage/user/blood.json'))
 
 let pendaftar = JSON.parse(fs.readFileSync('./storage/user/user.json'))
 let balance = JSON.parse(fs.readFileSync('./database/balance.json'))
@@ -106,7 +106,7 @@ var tgel = new Date();
 var hri = tgel.getDate();
 var bulnh = tgel.getMonth();
 var thisHari = tgel.getDay(),
-    thisDaye = myHari[thisHari];
+  thisDaye = myHari[thisHari];
 var yye = tgel.getYear();
 var syear = (yye < 1000) ? yye + 1900 : yye;
 const jangwak = (hri + '' + buln[bulnh] + '' + syear)
@@ -127,34 +127,56 @@ var yye = tgel.getYear();
 
 module.exports = Maria = async (Maria, m, chatUpdate, store) => {
   try {
-  var body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
-var budy = (typeof m.text == 'string' ? m.text : '')
-const prefix = global.prefa
-const isCmd = body.startsWith(prefix)
-const notCmd = body.startsWith('')
-const command = isCmd ? body.slice(1).trim().split(' ')[0].toLowerCase() : ''
-const args = body.trim().split(/ +/).slice(1)
-const pushname = m.pushName || "No Name"
+    var body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
+    var budy = (typeof m.text == 'string' ? m.text : '')
+    const prefix = global.prefa
+    const isCmd = body.startsWith(prefix)
+    const notCmd = body.startsWith('')
+    const command = isCmd ? body.slice(1).trim().split(' ')[0].toLowerCase() : ''
+    const args = body.trim().split(/ +/).slice(1)
+    const pushname = m.pushName || "No Name"
 
-const botNumber = await Maria.decodeJid(Maria.user.id)
-const isCreator = [botNumber, ...global.Owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
-const itsMe = m.sender == botNumber ? true : false
-const text = args.join(" ")
-const from = m.chat
-const quoted = m.quoted ? m.quoted : m
-const mime = (quoted.msg || quoted).mimetype || ''
-const isMedia = /image|video|sticker|audio/.test(mime)
-const messagesD = body.slice(0).trim().split(/ +/).shift().toLowerCase()
-const groupMetadata = m.isGroup ? await Maria.groupMetadata(m.chat).catch(e => {}) : ''
-const groupName = m.isGroup ? groupMetadata.subject : ''
-const participants = m.isGroup ? await groupMetadata.participants : ''
-const groupAdmins = m.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
-const groupOwner = m.isGroup ? groupMetadata.owner : ''
-const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
-const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
-const isUser = pendaftar.includes(m.sender)
-const isBan = banUser.includes(m.sender)
-   
+    const botNumber = await Miku.decodeJid(Miku.user.id)
+    const isCreator = [botNumber, ...global.Owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+    const itsMe = m.sender == botNumber ? true : false
+    const text = args.join(" ")
+    const from = m.chat
+    const quoted = m.quoted ? m.quoted : m
+    const mime = (quoted.msg || quoted).mimetype || ''
+    const isMedia = /image|video|sticker|audio/.test(mime)
+    const messagesD = body.slice(0).trim().split(/ +/).shift().toLowerCase()
+    const groupMetadata = m.isGroup ? await Miku.groupMetadata(m.chat).catch(e => { }) : ''
+    const groupName = m.isGroup ? groupMetadata.subject : ''
+    const participants = m.isGroup ? await groupMetadata.participants : ''
+    const groupAdmins = m.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
+    const groupOwner = m.isGroup ? groupMetadata.owner : ''
+    const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
+    const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+    const isUser = pendaftar.includes(m.sender)
+    const isBan = banUser.includes(m.sender)
+    const isBanChat = m.isGroup ? banchat.includes(from) : false
+    const isRakyat = isCreator || global.rkyt.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender) || false
+    const AntiLink = m.isGroup ? ntilink.includes(from) : false
+    const AntiLinkYoutubeVid = m.isGroup ? ntilinkytvid.includes(from) : false
+    const AntiLinkYoutubeChannel = m.isGroup ? ntilinkytch.includes(from) : false
+    const AntiLinkInstagram = m.isGroup ? ntilinkig.includes(from) : false
+    const AntiLinkFacebook = m.isGroup ? ntilinkfb.includes(from) : false
+    const AntiLinkTiktok = m.isGroup ? ntilinktt.includes(from) : false
+    const AntiLinkTelegram = m.isGroup ? ntilinktg.includes(from) : false
+    const AntiLinkTwitter = m.isGroup ? ntilinktwt.includes(from) : false
+    const AntiLinkAll = m.isGroup ? ntilinkall.includes(from) : false
+    const antiWame = m.isGroup ? ntwame.includes(from) : false
+    const antiVirtex = m.isGroup ? ntvirtex.includes(from) : false
+    const AntiNsfw = m.isGroup ? ntnsfw.includes(from) : false
+    const isLeveling = m.isGroup ? _leveling.includes(from) : false
+    autoreadsw = true
+    const content = JSON.stringify(m.message)
+    const q = args.join(' ')
+
+    const isQuotedVideo = m.mtype === 'extendedTextMessage' && content.includes('videoMessage')
+    const isQuotedAudio = m.mtype === 'extendedTextMessage' && content.includes('audioMessage')
+
+
 
     autoreadsw = true;
     _sewa.expiredCheck(Maria, sewa);
@@ -167,9 +189,9 @@ const isBan = banUser.includes(m.sender)
     /* const replay = (teks) => {
       Maria.sendMessage(m.chat, { text: teks }, { quoted: m }); 
     }; */
-const sender = m.isGroup ? (m.key.participant ? m.key.participant : m.participant) : m.key.remoteJid
-        const senderNumber = sender.split('@')[0]
-        
+    const sender = m.isGroup ? (m.key.participant ? m.key.participant : m.participant) : m.key.remoteJid
+    const senderNumber = sender.split('@')[0]
+
     function randomNomor(angka) {
       return Math.floor(Math.random() * angka) + 1;
     }
@@ -180,8 +202,8 @@ const sender = m.isGroup ? (m.key.participant ? m.key.participant : m.participan
         chalk.black(chalk.bgWhite("[ MESSAGE ]")),
         chalk.black(chalk.bgGreen(new Date())),
         chalk.black(chalk.bgBlue(budy || m.mtype)) +
-          "\n" +
-          chalk.magenta("=> From"),
+        "\n" +
+        chalk.magenta("=> From"),
         chalk.green(pushname),
         chalk.yellow(m.sender) + "\n" + chalk.blueBright("=> In"),
         chalk.green(m.isGroup ? pushname : "Private Chat", m.chat)
@@ -191,12 +213,6 @@ const sender = m.isGroup ? (m.key.participant ? m.key.participant : m.participan
     if (isCmd && !isUser) {
       pendaftar.push(m.sender);
       fs.writeFileSync("./storage/user/user.json", JSON.stringify(pendaftar));
-    }
-
-    if (autoreadsw) {
-      if (from === "status@broadcast") {
-        Maria.chatRead(from);
-      }
     }
 
     if (global.autoreadpmngc) {
@@ -268,89 +284,103 @@ const sender = m.isGroup ? (m.key.participant ? m.key.participant : m.participan
       );
     }
 
- switch (command) {
-    //General commands
-        case "rules":
-    const helptxt=`_*📍[Rules for Maria Md usage]📍*_\n\n\n*>>>* use -support to get the Official group link in your dm.\n\n*--->* If you want to add Maria-Md in your group the contact the owner by *-owner/-mods* \n\n*--->* Dont use wrong command, use the command given in the *-help* list \n\n* Dont spam the bot with commands if Maria-Md is not responding, its means the maybe owner is offline or facing internet issue. \n\n* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BANNED* 🚫 \n\n\n*©️ Ayush Bots inc* `
-      
-   Maria.sendMessage(from, { video: { url: 'https://c.tenor.com/geMdtLCXZkAAAAPo/rules.mp4' }, gifPlayback: true, caption: helptxt }, { quoted: m })
-    
-    break
-        case 'hii': case 'hi': case 'Hi':
-let txxt= `👋🏻 Hi *${pushname}*, i am  *Maria-Md*📍. A whatsapp bot created by: Ayush and based on Maria  bot: FantoX001. I don't have time for chit-chat Darling. Use command from *${prefix}help* list if you want me to do anything.`
+    if (AntiLinkAll)
+      if (budy.includes("https://")) {
+        if (!isBotAdmins) return
+        bvl = `\`\`\`「  Antilink System  」\`\`\`\n\nLink sent by Admin so no action will be taken!`
+        if (isAdmins) return reply(bvl)
+        if (m.key.fromMe) return reply(bvl)
+        if (isCreator) return reply(bvl)
+        kice = m.sender
+        await Miku.groupParticipantsUpdate(m.chat, [kice], 'remove')
+        Miku.sendMessage(from, { text: `\`\`\`「  Antilink System  」\`\`\`\n\n@${kice.split("@")[0]} Baka has been removed for sending links in this group!`, contextInfo: { mentionedJid: [kice] } }, { quoted: m })
+      } else {
+      }
 
-Maria.sendMessage(m.chat, { video: {url: `https://c.tenor.com/KqyGziKFp6wAAAPo/zero-two-live-wallpaper.mp4`},caption: txxt, gifPlayback:true }, { quoted: m });
-break 
-case "support":
-    
-          let tex = `📍My Developer's Group📍\n\n*🎇 𝐌𝐚𝐫𝐢𝐚 support group:🎇* *https://chat.whatsapp.com/FoS7pSPtfMqBuoireK4aAJ*`
+    switch (command) {
+      //General commands
+      case "rules":
+        const helptxt = `_*📍[Rules for Maria Md usage]📍*_\n\n\n*>>>* use -support to get the Official group link in your dm.\n\n*--->* If you want to add Maria-Md in your group the contact the owner by *-owner/-mods* \n\n*--->* Dont use wrong command, use the command given in the *-help* list \n\n* Dont spam the bot with commands if Maria-Md is not responding, its means the maybe owner is offline or facing internet issue. \n\n* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BANNED* 🚫 \n\n\n*©️ Ayush Bots inc* `
 
-  await Maria.sendMessage(m.sender,  { text: `${tex}` },);
-          
-         await Maria.sendMessage(m.chat, { video: {url: `https://c.tenor.com/3D__CbAhTl8AAAPo/girl-cute.mp4`},caption: 'I sent you the support Link in personal message.\n Pls check.', gifPlayback:true }, { quoted: m });
-          break
-          
-          case "info":
-          let ifx = `『𝕄𝕒𝕣𝕚𝕒-𝕄𝕕 』\n\n🔮 *Total Groups: 
+        Maria.sendMessage(from, { video: { url: 'https://c.tenor.com/geMdtLCXZkAAAAPo/rules.mp4' }, gifPlayback: true, caption: helptxt }, { quoted: m })
+
+        break
+      case 'hii': case 'hi': case 'Hi':
+        let txxt = `👋🏻 Hi *${pushname}*, i am  *Maria-Md*📍. A whatsapp bot created by: Ayush and based on Maria  bot: FantoX001. I don't have time for chit-chat Darling. Use command from *${prefix}help* list if you want me to do anything.`
+
+        Maria.sendMessage(m.chat, { video: { url: `https://c.tenor.com/KqyGziKFp6wAAAPo/zero-two-live-wallpaper.mp4` }, caption: txxt, gifPlayback: true }, { quoted: m });
+        break
+      case "support":
+
+        let tex = `📍My Developer's Group📍\n\n*🎇 𝐌𝐚𝐫𝐢𝐚 support group:🎇* *https://chat.whatsapp.com/FoS7pSPtfMqBuoireK4aAJ*`
+
+        await Maria.sendMessage(m.sender, { text: `${tex}` },);
+
+        await Maria.sendMessage(m.chat, { video: { url: `https://c.tenor.com/3D__CbAhTl8AAAPo/girl-cute.mp4` }, caption: 'I sent you the support Link in personal message.\n Pls check.', gifPlayback: true }, { quoted: m });
+        break
+
+      case "info":
+        let ifx = `『𝕄𝕒𝕣𝕚𝕒-𝕄𝕕 』\n\n🔮 *Total Groups: 
 				chats.length
 					\n\n📍 *Last Re-booted:${runtime(process.uptime())} *📍 Total Users: users\n\n💢  *Total Banned Users: uban\n\n\n  *©️ Ayush Bots*\n\n`
-			break
-			
-			case 'owner': case 'creator': case 'mod': case 'mods':{
-   Maria.sendContact(m.chat, global.Owner, m)}
-    
-    break
-    case 'remove':{
-        
-     if (!m.isGroup) return replay(mess.grouponly)
-     if (!isBotAdmins) return replay(mess.botadmin)
-     if (!isAdmins && !isCreator) return replay(mess.useradmin)
-     let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-     await Maria.groupParticipantsUpdate(m.chat, [users], 'remove')
-     }
-     break
- ///////////////////////////////////////////////////
-		
-			//Group menu
-			
-	case 'add':{     			
-    if (!m.isGroup) return replay(mess.grouponly)
- if (!isBotAdmins) return replay(mess.botadmin)
- let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
- if (users.length == 0) return replay(`Please write the number of the person you want to add to thhis group`)
-  await Maria.groupParticipantsUpdate(m.chat, [users], 'add').then((res) => replay(`User Added Successfully!`)).catch((err) => replay(`Cannot add that user to this group!`))
- }
- break
-			
-		//////search menu
-		
- case 'weather':
+        break
+
+      case 'owner': case 'creator': case 'mod': case 'mods': {
+        Maria.sendContact(m.chat, global.Owner, m)
+      }
+
+        break
+      case 'remove': {
+
+        if (!m.isGroup) return replay(mess.grouponly)
+        if (!isBotAdmins) return replay(mess.botadmin)
+        if (!isAdmins && !isCreator) return replay(mess.useradmin)
+        let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+        await Maria.groupParticipantsUpdate(m.chat, [users], 'remove')
+      }
+        break
+      ///////////////////////////////////////////////////
+
+      //Group menu
+
+      case 'add': {
+        if (!m.isGroup) return replay(mess.grouponly)
+        if (!isBotAdmins) return replay(mess.botadmin)
+        let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+        if (users.length == 0) return replay(`Please write the number of the person you want to add to thhis group`)
+        await Maria.groupParticipantsUpdate(m.chat, [users], 'add').then((res) => replay(`User Added Successfully!`)).catch((err) => replay(`Cannot add that user to this group!`))
+      }
+        break
+
+      //////search menu
+
+      case 'weather':
 
         if (!args[0]) return reply("Enter your location to search weather.")
-         myweather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args.join(" ")}&units=metric&appid=e409825a497a0c894d2dd975542234b0&language=tr`)
+        myweather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args.join(" ")}&units=metric&appid=e409825a497a0c894d2dd975542234b0&language=tr`)
 
         const weathertext = `           🌤 *Weather Report* 🌤  \n\n🔎 *Search Location:* ${myweather.data.name}\n*💮 Country:* ${myweather.data.sys.country}\n🌈 *Weather:* ${myweather.data.weather[0].description}\n🌡️ *Temperature:* ${myweather.data.main.temp}°C\n❄️ *Minimum Temperature:* ${myweather.data.main.temp_min}°C\n📛 *Maximum Temperature:* ${myweather.data.main.temp_max}°C\n💦 *Humidity:* ${myweather.data.main.humidity}%\n🎐 *Wind:* ${myweather.data.wind.speed} km/h\n`
         Maria.sendMessage(from, { video: { url: 'https://media.tenor.com/bC57J4v11UcAAAPo/weather-sunny.mp4' }, gifPlayback: true, caption: weathertext }, { quoted: m })
 
         break
-        
- 
- ///////////////////////////////////////////////////
- ///funmenu
- 
- case 'stupidcheck':case 'uncleancheck':
-case 'hotcheck': case 'smartcheck':
-case 'greatcheck':
-case 'evilcheck':case 'dogcheck':
-case 'coolcheck':
-case 'waifucheck':
-cantik = body.slice(1)
-const okebnh1 =['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
-const Mariakak = okebnh1[Math.floor(Math.random() * okebnh1.length)]
-Maria.sendMessage(m.chat, { text: Mariakak }, { quoted: m })
-break
 
- ///////////////////////////////////////////////////
+
+      ///////////////////////////////////////////////////
+      ///funmenu
+
+      case 'stupidcheck': case 'uncleancheck':
+      case 'hotcheck': case 'smartcheck':
+      case 'greatcheck':
+      case 'evilcheck': case 'dogcheck':
+      case 'coolcheck':
+      case 'waifucheck':
+        cantik = body.slice(1)
+        const okebnh1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100']
+        const Mariakak = okebnh1[Math.floor(Math.random() * okebnh1.length)]
+        Maria.sendMessage(m.chat, { text: Mariakak }, { quoted: m })
+        break
+
+      ///////////////////////////////////////////////////
 
       default:
         if (budy.startsWith("=>")) {
